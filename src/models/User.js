8cc6f1b94c0ec010f01c65c9f56d0b6154684c8f
Ast-ai25 <inditech1978@gmail.com>
2025-06-19 15:06:46 +0530

    fix: add Role association to User

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
+const Role = require('./Role');
 
 const User = sequelize.define('User', {
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
@@ -11,5 +12,8 @@ const User = sequelize.define('User', {
     role_id: { type: DataTypes.INTEGER, allowNull: false },
     is_active: { type: DataTypes.BOOLEAN, defaultValue: true }
 });
+
+// Define Relationships
+User.belongsTo(Role, { foreignKey: 'role_id', as: 'Role' });
 
 module.exports = User;
